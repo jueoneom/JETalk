edb15bf59ba46dbb495d1e5e7a5f5b195ef65cf7
jueoneom <lluot.llu@gmail.com>
2020-06-24 22:42:34 +0900

    update firebase setting

---
+++ JETalk/JETalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JETalk
+//
+//  Created by JE on 2020/06/24.
+//  Copyright © 2020 JE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
