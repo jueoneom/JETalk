ba826bf94d893b2183070501bebfe82931932847
jueoneom <lluot.llu@gmail.com>
2020-06-26 06:05:13 +0900

    update StoryBoard & select Room 기능 구현

---
+++ JETalk/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  JETalk
+//
+//  Created by JE on 2020/06/26.
+//  Copyright © 2020 JE. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var mainBtnCell: UIButton!
+    
+}
---
+++ JETalk/DetailVC.swift
@@ -0,0 +1,24 @@
+//
+//  DetailVC.swift
+//  JETalk
+//
+//  Created by JE on 2020/06/26.
+//  Copyright © 2020 JE. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    var dataName : String = ""
+    var imgName : String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
